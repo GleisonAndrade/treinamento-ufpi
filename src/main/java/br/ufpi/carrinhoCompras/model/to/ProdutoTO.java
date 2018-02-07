f01232459b856e70bb43d9ecc4b4a7b173d8034c
gleison <gleisondeandradeesilva@gmail.com>
2018-02-07 14:31:41 -0300

    Adição exemplo ode WEBSERVICE

---
+++ src/main/java/br/ufpi/carrinhoCompras/model/to/ProdutoTO.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package br.ufpi.carrinhoCompras.model.to;
+
+import java.io.Serializable;
+
+/**
+ * @author gleison
+ *
+ */
+public class ProdutoTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String descricao;
+	private Double preco;
+	private String tipo;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the descricao
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+
+	/**
+	 * @param descricao
+	 *            the descricao to set
+	 */
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	/**
+	 * @return the preco
+	 */
+	public Double getPreco() {
+		return preco;
+	}
+
+	/**
+	 * @param preco
+	 *            the preco to set
+	 */
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	/**
+	 * @return the tipo
+	 */
+	public String getTipo() {
+		return tipo;
+	}
+
+	/**
+	 * @param tipo
+	 *            the tipo to set
+	 */
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
---
+++ src/main/java/br/ufpi/carrinhoCompras/webservice/WBCorreios.java
@@ -0,0 +1,131 @@
+/**
+ * 
+ */
+package br.ufpi.carrinhoCompras.webservice;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import br.com.techzee.correios.ws.ConsultaCorreios;
+import br.com.techzee.correios.ws.dto.CorreiosPrecoPrazo;
+import br.com.techzee.correios.ws.enumeration.CorreiosTipoServico;
+import br.com.techzee.correios.ws.enumeration.IndicadorSN;
+
+/**
+ * @author gleison
+ *
+ */
+
+@Named
+@RequestScoped
+public class WBCorreios implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private CorreiosPrecoPrazo result;
+	private String cepOrigem = "64004325";
+	private String cepDestino;
+	private String prazo;
+	private String preco;
+	private boolean renderized= false;
+	
+	public WBCorreios() {
+	}
+	
+	public void mostrarFrete() {
+		System.out.println("mostrarFrete()");
+		this.renderized = !renderized;
+	}
+	
+	public void calculaPrazoEntrega() {
+		result = calculaPrazoEntrega2(CorreiosTipoServico.PAC_VAREJO, cepOrigem, cepDestino);
+		this.prazo = result.getPrazoEntrega().toString();
+		this.preco = result.getPrecoEntregaEmMaos().toString();
+		
+		System.out.println("Preço: " + preco + ", Prazo: " + prazo);
+	}
+	
+	public CorreiosPrecoPrazo calculaPrazoEntrega2(CorreiosTipoServico tipoServico, String cepOrigem, String cepDestino) {
+		try {
+			result = new ConsultaCorreios()
+					.servicos(tipoServico) //tipo de serviço 'PAC'
+					.entregarEmMaos(IndicadorSN.SIM) //indicador que define se a entrega deve ser em mãos
+					.calcularPrecoPrazo(cepOrigem, cepDestino)[0];
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
+	/**
+	 * @return the cepOrigem
+	 */
+	public String getCepOrigem() {
+		return cepOrigem;
+	}
+
+	/**
+	 * @param cepOrigem the cepOrigem to set
+	 */
+	public void setCepOrigem(String cepOrigem) {
+		this.cepOrigem = cepOrigem;
+	}
+
+	/**
+	 * @return the cepDestino
+	 */
+	public String getCepDestino() {
+		return cepDestino;
+	}
+
+	/**
+	 * @param cepDestino the cepDestino to set
+	 */
+	public void setCepDestino(String cepDestino) {
+		this.cepDestino = cepDestino;
+	}
+
+	/**
+	 * @return the prazo
+	 */
+	public String getPrazo() {
+		return prazo;
+	}
+
+	/**
+	 * @param prazo the prazo to set
+	 */
+	public void setPrazo(String prazo) {
+		this.prazo = prazo;
+	}
+
+	/**
+	 * @return the preco
+	 */
+	public String getPreco() {
+		return preco;
+	}
+
+	/**
+	 * @param preco the preco to set
+	 */
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+
+	/**
+	 * @return the renderized
+	 */
+	public boolean isRenderized() {
+		return renderized;
+	}
+
+	/**
+	 * @param renderized the renderized to set
+	 */
+	public void setRenderized(boolean renderized) {
+		this.renderized = renderized;
+	}
+}
